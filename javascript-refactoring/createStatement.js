21c6f26f0e244e39246ec2fa0fae12cbe94d6be7
Joscha Götzer <joscha.goetzer@gmail.com>
2022-07-23 14:03:50 +0200

    Refactored for better readability with typings

---
+++ javascript-refactoring/createStatement.js
@@ -0,0 +1,109 @@
+/**
+ *
+ * @param {
+ *     {
+ *         customer: string,
+ *         performances: {
+ *             playID: string,
+ *             audience: number
+ *         }[]
+ *     }
+ * } invoice
+ * @param {
+ *     {
+ *         [key: string]: {
+ *               name: string,
+ *               type: string
+ *         }
+ *     }
+ * } plays
+ * @returns {string}
+ */
+function createStatement(invoice, plays) {
+    const summary = invoice.performances.reduce(
+        (current, perf) => {
+            const audience = perf.audience;
+            const play = plays[perf.playID];
+            const playType = play.type;
+
+            const item = calculateOrderItem(playType, audience);
+
+            return {
+                total: current.total + item,
+                volumeCredits:
+                    current.volumeCredits +
+                    calculateVolumeCredits(playType, audience),
+                orders: {
+                    ...current.orders,
+                    [play.name]: { item, audience },
+                },
+            };
+        },
+        { total: 0, volumeCredits: 0, orders: {} }
+    );
+
+    const parts = [`Statement for ${invoice.customer}`];
+
+    Object.entries(summary.orders).forEach(([key, value]) =>
+        parts.push(
+            `    ${key}: ${formatCurrency(value.item / 100)} (${
+                value.audience
+            } seats)`
+        )
+    );
+
+    parts.push(`  Amount owed is ${formatCurrency(summary.total / 100)}`);
+    parts.push(`  You earned ${summary.volumeCredits} credits`);
+    return parts.join("\n");
+}
+
+/**
+ * @param {string} playType
+ * @param {number} audience
+ * @returns {number}
+ */
+const calculateOrderItem = (playType, audience) => {
+    switch (playType) {
+        case "tragedy":
+            return 40000 + (audience > 30 ? 1000 * (audience - 30) : 0);
+        case "comedy":
+            return (
+                30000 +
+                300 * audience +
+                (audience > 20 ? 10000 + 500 * (audience - 20) : 0)
+            );
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+};
+
+/**
+ * @param {string} playType
+ * @param {number} audience
+ * @returns {number}
+ */
+const calculateVolumeCredits = (playType, audience) => {
+    let result = Math.max(audience - 30, 0);
+
+    // add extra credit for every ten comedy attendees
+    if (playType === "comedy") {
+        result += Math.floor(audience / 5);
+    }
+
+    return result;
+};
+
+/**
+ * @param {number} value
+ * @returns {string}
+ */
+const formatCurrency = (value) =>
+    new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    }).format(value);
+
+module.exports = {
+    createStatement,
+};
--- javascript-refactoring/index.js
+++ javascript-refactoring/index.js
@@ -1,5 +1,5 @@
-const { statement } = require("./statement")
+const { createStatement } = require("./createStatement")
 const plays = require("./plays.json")
 const invoices = require("./invoices.json")
 
-console.log(statement(invoices[0], plays));
+console.log(createStatement(invoices[0], plays));
